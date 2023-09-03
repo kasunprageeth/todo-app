2e5fde5ae96a6fdb405ccd243b4e938705d8d88f
Kasun Karunathilaka <kasun@blackswan-technologies.com>
2023-09-04 00:18:15 +0530

    add new component and create new ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { CategorieComponent } from './categorie/categorie.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    CategorieComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
